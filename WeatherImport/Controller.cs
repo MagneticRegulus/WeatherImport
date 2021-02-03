10f9c658a81c045f3c188cff8b1f385dbd1f69e0
MagneticRegulus <ladymeckbert@gmail.com>
2021-02-03 22:28:07 +1300

    Load Month data and load days in theory

--- WeatherImport/Controller.cs
+++ WeatherImport/Controller.cs
@@ -10,16 +10,10 @@ namespace WeatherImport
     class Controller
     {
         public List<Month> Months { get; set; }
-        public Day Day { get; set; }
-        public Month Month { get; set; }
 
         public Controller()
         {
-            Console.WriteLine("Controller Added");
-            Day = new Day();
-            Month = new Month();
-            Month.Days.Add(Day);
-            Console.WriteLine("Day added to month");
+            Months = new List<Month>();
             string filename = System.IO.Path.GetFullPath(Directory.GetCurrentDirectory() + @"\\data\\weather.txt");
             ReadFile(filename);
             Console.Read();
@@ -30,10 +24,38 @@ namespace WeatherImport
         {
             try
             {
+                Month filemonth = null;
+                List<string> data = new List<string>();
                 string[] lines = System.IO.File.ReadAllLines(filename);
-                foreach (string s in lines)
+                foreach (string curr in lines)
                 {
-                    Console.WriteLine(s);
+                    if (curr.StartsWith(" ") && !curr.StartsWith("\t")) //check if line starts with space && is not blank
+                    {
+                        string[] cols = curr.Trim().Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                        if (cols[0] == "MMU") //add month
+                        {
+                            filemonth = new Month(cols[1], Int32.Parse(cols[2]));
+                            Months.Add(filemonth);
+                        } else if (cols[0] == "mo" && cols.Length == 9 && filemonth != null) //add month attributes
+                        {
+                            filemonth.AvMxT = Double.Parse(cols[1]);
+                            filemonth.AvMnT = Double.Parse(cols[2]);
+                            filemonth.AvAvT = Double.Parse(cols[3]);
+                            filemonth.TotHDDay = Int32.Parse(cols[4]);
+                            filemonth.AvAvDP = Double.Parse(cols[5]);
+                            filemonth.MnTPcpn = Double.Parse(cols[6]);
+                            filemonth.AvAvSp = Double.Parse(cols[7]);
+                            filemonth.AvSkyC = Double.Parse(cols[8]);
+                            //debug: Console.WriteLine("Added all columns");
+                        } else if (cols[0] != "Dy")
+                        {
+                            filemonth.Days.Add(new Day(curr));
+                        }
+                    }
+                }
+                foreach (Month m in Months)
+                {
+                    Console.WriteLine(m.Title + " " + m.Year + " loaded with " + m.Days.Count + " days");
                 }
 
             } catch (System.Exception e)
--- WeatherImport/Month.cs
+++ WeatherImport/Month.cs
@@ -9,13 +9,13 @@ namespace WeatherImport
     class Month
     {
         public List<Day> Days { get; set; }
-        public string MonthName { get; set; }
+        public string Title { get; set; }
         public int Year { get; set; }
         public double AvMxT { get; set; } //average?
         public double AvMnT { get; set; } //average?
         public double AvAvT { get; set; } //average?
         public int TotHDDay { get; set; } //total?
-        public double AvDP { get; set; } //average?
+        public double AvAvDP { get; set; } //average?
         public double MnTPcpn { get; set; } //unclear if total or average
         public double AvAvSp { get; set; } //average?
         public double AvSkyC { get; set; } //average?
@@ -24,8 +24,10 @@ namespace WeatherImport
         public Day MnTDay { get; set; }
         public Day MxSDay { get; set; }
 
-        public Month()
+        public Month(string title, int year)
         {
+            Title = title;
+            Year = year;
             Days = new List<Day>();
             Console.WriteLine("Month Added");
         }
